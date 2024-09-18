53077101e3eb98a9c1cf13e716a83f43edbdd5f1
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-09-18 20:04:14 -0300

    Adicionado css dentro da tag section

--- form-app/src/pages/_app.tsx
+++ form-app/src/pages/_app.tsx
@@ -3,8 +3,8 @@ import "@/styles/globals.css";
 
 export default function App() {
   return(
-    <>
-
-    </>
+    <section className="flex w-screen h-screen bg-lime-200">
+      
+    </section>
   );
 }

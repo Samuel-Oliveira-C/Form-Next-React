9f60b2d59f7514ad3e95529273e243c236b891c6
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-09-18 21:16:17 -0300

    Terminada a parte de estilização, agora falta a responsividade

--- form-app/src/pages/componentes/FormComponent.tsx
+++ form-app/src/pages/componentes/FormComponent.tsx
@@ -6,22 +6,34 @@ const FormComponent = () =>{
 
 
     return(
-        <form className="bg-slate-500 w-2/5 h-2/3 flex flex-col gap-2">
+        <form className="bg-slate-500 w-1/3 h-2/5 flex flex-col gap-3 ">
+            <div>
+                <h1 className="text-center text-2xl font-bold">Login</h1>
+                <div className="w-full h-px bg-black"></div>
+            </div>
+            <div className="flex flex-col gap-7">
             <input 
+                className="rounded-xl p-2 "
                 type="email"
                 placeholder="Email"
                 value={input}
                 onChange={key => setInputState(key.target.value)}
             />
             <input 
+                className="rounded-xl p-2 "
                 type="password" 
                 placeholder="Senha"
                 value={inputPassword}
                 onChange={key => setPasswordState(key.target.value)}
             />
-            <a href="">Esqueceu a Senha ?</a>
-
-            <button>Entrar</button>
+            </div>
+            <div className="flex justify-end">
+                <a href="" className="mr-3 text-fuchsia-50 ">
+                    Esqueceu a Senha ?</a>
+            </div>
+            <div className="flex justify-center">
+            <button className="bg-amber-400 rounded-lg w-24 p-1">Entrar</button>
+            </div>
         </form>
     )
 }
@@ -31,4 +43,5 @@ export{FormComponent}
 /** Como não Pretendo passar esses dados não utilizarei o atributo "action"
  *  Quanto a validação deles posso pensar nisso depois.
  *  O intuito é utilizar os conceitos da aula.
+ *  //TODO falta colocar Responsividade 
  */
